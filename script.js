8ce3a71ff28cc3d6b8c590581bcd63c0cf1a5d39
Nuel Elekwachi <elekwaeke300@gmail.com>
2023-06-01 20:14:58 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const labelName = document.getElementById("label_name");
+const cardNumber = document.getElementById("card_number");
+const title = document.querySelector(".title");
+const zeros = document.querySelector(".zeros");
+
+labelName.addEventListener("input", (e) => {
+  const input = e.target.value;
+  title.textContent = input;
+  console.log("worked");
+});
+
+cardNumber.addEventListener("input", (e) => {
+  const input = e.target.value;
+  zeros.textContent = input;
+  console.log("worked");
+});
